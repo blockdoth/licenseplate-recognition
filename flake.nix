4be6d58aeea99299023dda9d7ac053e0747c5c10
blockdoth <pepijn.pve@gmail.com>
2024-11-18 09:05:14 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A Nix flake default template for a development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            shellHook = "
+            echo 'Entering a default shell template'
+          ";
+            packages = with pkgs; [
+
+            ];
+          };
+        }
+      );
+    };
+}
+
